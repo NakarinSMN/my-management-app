3df1d80ae81032d928118f1c60741ef0317bc4f2
NakarinSMN <chuaisangmanee.changger@gmail.com>
2025-11-20 09:48:34 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Netlify plugin will handle the output configuration
 };
 
 export default nextConfig;
